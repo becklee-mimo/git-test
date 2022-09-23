3c7a07832c79e5394ab50c9ae3dde5a64e0522bb
beck <becklee@outlook.com>
2022-09-23 16:00:18 +0800

    hot-fix commit

--- src/main/java/com/becklee/git/GitTest.java
+++ src/main/java/com/becklee/git/GitTest.java
@@ -4,6 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git test");
         System.out.println("hello,git test");
+        System.out.println("hello,git test");
+        System.out.println("hello,git test hot-fix");
 
     }
 }
