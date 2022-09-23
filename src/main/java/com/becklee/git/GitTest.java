b2d9a03b6680e5096b2a787ab0a97fbef1725789
beck <becklee@outlook.com>
2022-09-23 12:41:31 +0800

    second commit

--- src/main/java/com/becklee/git/GitTest.java
+++ src/main/java/com/becklee/git/GitTest.java
@@ -3,5 +3,7 @@ package com.becklee.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git test");
+        System.out.println("hello,git test");
+
     }
 }
